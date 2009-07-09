cd173a79b9c10c6279a06d87aff0636b7c7e6e7b
megarushing <Megarushing@users.noreply.github.com>
2009-07-09 05:38:42 +0000

    adicionado PSDs

--- setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-import py2exe
-import sys
-
-sys.argv.append("py2exe")
-
-setup(windows=[{'script': 'cicfighters.py', 'icon_resources': [(1, "cicfighters-large.ico")]}],
-     options = {'py2exe': {'optimize': 1, 'bundle_files':1} },
-     zipfile=None)
